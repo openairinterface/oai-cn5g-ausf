2635c4dedc4ac8ad6f3ea57936d955a9fbd4c5d4
Raphael Defosseux <raphael.defosseux@eurecom.fr>
2021-06-03 11:23:02 +0200

    CLANG-FORMAT: a few missing files were not formatted
    Signed-off-by: Raphael Defosseux <raphael.defosseux@eurecom.fr>

--- src/common/utils/mime_parser.cpp
+++ src/common/utils/mime_parser.cpp
@@ -57,7 +57,7 @@ bool mime_parser::parse(const std::string& str) {
         str.substr(content_type_pos + 14, crlf_pos - (content_type_pos + 14));
     Logger::ausf_server().debug("Content Type: %s", p.content_type.c_str());
 
-    crlf_pos     = str.find(CRLF + CRLF, content_type_pos);  // beginning of content
+    crlf_pos = str.find(CRLF + CRLF, content_type_pos);  // beginning of content
     boundary_pos = str.find(boundary_full, crlf_pos);
     if (boundary_pos == std::string::npos) {
       boundary_pos = str.find(last_boundary, crlf_pos);
