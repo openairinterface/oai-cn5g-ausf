fe4e4463a8f3edb78ac7059eb79db36ae9b24d03
Raphael Defosseux <raphael.defosseux@eurecom.fr>
2023-01-17 11:08:06 +0000

    feat(config): changing the configuration file to jinja2-based approach

--- ci-scripts/flatten_image.py
+++ ci-scripts/flatten_image.py
@@ -78,7 +78,7 @@ def perform_flattening(tag):
     cmd += ' --change "EXPOSE 80/tcp" '
     cmd += ' --change "HEALTHCHECK --interval=10s --timeout=15s --retries=6 CMD /openair-ausf/bin/healthcheck.sh" '
     cmd += ' --change "CMD [\\"/openair-ausf/bin/oai_ausf\\", \\"-c\\", \\"/openair-ausf/etc/ausf.conf\\", \\"-o\\"]" '
-    cmd += ' --change "ENTRYPOINT [\\"/bin/bash\\", \\"/openair-ausf/bin/entrypoint.sh\\"]" '
+    cmd += ' --change "ENTRYPOINT [\\"python3\\", \\"/openair-ausf/bin/entrypoint.py\\"]" '
     cmd += ' - ' + image_prefix + tag
     print (cmd)
     subprocess.check_output(cmd, shell=True, universal_newlines=True)
