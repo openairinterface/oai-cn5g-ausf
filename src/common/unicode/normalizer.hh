04f239b74922068e02b8becf9ead972c9c3f4fc0
Shubhika Garg <Shubhika.Garg@Openairinterface.org>
2026-03-25 11:01:00 +0100

    chore(license): Update OAI License to Collaborative Standards Software License v1.0 (CSSL)

--- src/common/unicode/normalizer.hh
+++ src/common/unicode/normalizer.hh
@@ -1,3 +1,6 @@
+/*
+ * SPDX-License-Identifier: LicenseRef-CSSL-1.0
+ */
 #ifndef UNF_NORMALIZER_HH
 #define UNF_NORMALIZER_HH
 
--- src/common/unicode/table.hh
+++ src/common/unicode/table.hh
@@ -1,3 +1,6 @@
+/*
+ * SPDX-License-Identifier: LicenseRef-CSSL-1.0
+ */
 #ifndef UNF_TABLE_HH
 #define UNF_TABLE_HH
 namespace UNF {
